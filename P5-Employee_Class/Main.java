4c1cc3143ae06a04d3834d9970c084ce32d96910
alardcollege12 <virajmore391@gmail.com>
2026-05-03 14:40:52 +0530

    Update Main.java

--- P5-Employee_Class/Main.java
+++ P5-Employee_Class/Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         Employee e = new Employee();
         e.setId(1);
-        e.setName("Sujal");
+        e.setName("Ram");
         e.setSalary(50000);
         e.display();
     }
