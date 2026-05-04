74aaf2451a65ea0c57f3d046137c5b2eaf696fa8
alardcollege12 <virajmore391@gmail.com>
2026-05-04 07:47:03 +0530

    Update Main.java

--- P24-Product_Price_Validation/Main.java
+++ P24-Product_Price_Validation/Main.java
@@ -2,17 +2,22 @@ class Product {
     private int id;
     private String name;
     private double price;
-    void setPrice(double price){
-        if(price>=0) this.price=price;
+
+    void set(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        if (price >= 0) this.price = price;
     }
-    void display(){
-        System.out.println(id+" "+name+" "+price);
+
+    void display() {
+        System.out.println(id + " " + name + " " + price);
     }
 }
+
 public class Main {
     public static void main(String[] args) {
-        Product p=new Product();
-        p.setPrice(100);
+        Product p = new Product();
+        p.set(101, "Book", 100);
         p.display();
     }
 }
